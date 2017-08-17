fe06b876c36a78f55aa023a158e39aa638feb1a4
Shawn Perry <shawn@redmop.com>
2017-08-17 12:23:05 -0600

    Update clamscan_weekly.sh

--- clamscan_weekly.sh
+++ clamscan_weekly.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
- 
+
 # email subject
 SUBJECT="VIRUS DETECTED ON $(hostname -f)!!!"
 # Email To ?
 EMAIL="USER@EXAMPLE.ORG"
 # Log location
-LOG=/var/log/clamav/scan.log
+LOG=/var/log/clamav/clamscan-$(date +'%Y-%m-%d').log
+# Excluded Directories
 EXCLUDES=""
 
 echo "" >> ${LOG}
-echo "***Start $0 at $(date)" >> ${LOG}
- 
+echo "-- Start $0 at $(date)" >> ${LOG}
+echo "" >> ${LOG}
+
 check_scan () {
 
     # Check the last set of results. If there are any "Infected" counts that aren't zero, we have a problem.
-    if [ `tail -n 12 ${LOG}  | grep Infected | grep -v 0 | wc -l` != 0 ]
+    if [ $(grep "Infected files:" ${LOG}  |grep -v "Infected files: 0" | wc -l) != 0 ]
     then
+        echo "" >> ${LOG}
+        echo "-- Infection detected, sending alert to $EMAIL" >> ${LOG}
+        echo "" >> ${LOG}
         EMAILMESSAGE=$(mktemp /tmp/virus-alert.XXXXX)
         echo "To: ${EMAIL}" >>  ${EMAILMESSAGE}
         echo "From: root@$(hostname -f)" >>  ${EMAILMESSAGE}
@@ -30,13 +35,38 @@ check_scan () {
 
 }
 
-for X in $EXCLUDES ; do
-        FULL_EXCLUDES="$FULL_EXCLUDES --exclude-dir=$X/"
+# Update ClamAV database
+echo >> $LOG;
+echo "-- Looking for ClamAV database updates at $(date)" >> $LOG;
+echo >> $LOG;
+/usr/bin/freshclam >> $LOG;
+echo >> $LOG;
+
+
+# Build Exclude Lists
+for X in $SCAN_EXCLUDE_DIR ; do
+        FULL_EXCLUDES="$FULL_EXCLUDES --exclude-dir=$X"
 done
 
-clamscan -r / --exclude-dir=/sys/ --quiet --infected --log=${LOG}
- 
+echo "" >> ${LOG}
+echo "-- Clamscan started at $(date)" >> ${LOG}
+echo "" >> ${LOG}
+echo "Command line: time clamscan -r / --exclude-dir=/sys/ $FULL_EXCLUDES --quiet --infected --log=${LOG} --cross-fs=no" >> ${LOG}
+echo "" >> ${LOG}
+
+time clamscan -r / --exclude-dir=/sys/ $FULL_EXCLUDES --quiet --infected --log=${LOG} --cross-fs=no
 check_scan
 
+echo "" >> ${LOG}
+echo "-- Scan Complete $(date)" >> ${LOG}
+echo "" >> ${LOG}
+
+echo "" >> ${LOG}
+echo "-- Cleaning Log Files at $(date)" >> ${LOG}
+echo "" >> ${LOG}
+
+find /var/log/clamav -mtime 30 -exec rm -v {} \;
+
+echo "" >> ${LOG}
 echo "***End $0 at $(date)" >> ${LOG}
 echo "" >> ${LOG}
